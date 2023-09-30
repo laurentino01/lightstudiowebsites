a200476d6af971322341ea68b4f8ead49635eaec
Gabriel Laurentino <gabriellaurentino@hotmail.com>
2023-09-30 16:38:09 -0300

    init

---
+++ src/layouts/Menu.style.ts
@@ -0,0 +1,7 @@
+const container =
+  "bg-dark pr-32 pl-16 flex flex-col gap-0 items-center justify-center shrink-0 w-[1440px] h-[72px] relative overflow-hidden";
+
+const nav =
+  "bg-dark flex flex-row gap-8 items-center justify-center shrink-0 relative";
+
+export { container, nav };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,8 +10,8 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        primary: "#566ec1",
         dark: "#1f1f1f",
+        primary: "#566ec1",
         light: "#fbfbfb",
       },
     },
